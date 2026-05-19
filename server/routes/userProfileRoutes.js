8580c05be7c5a0a9bac2ca4534cf853c3619705e
Luana Alves Camilo <133168997+luanacamilo@users.noreply.github.com>
2026-05-19 14:17:51 -0300

    .

--- server/routes/userProfileRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userProfileController = require('../controllers/userProfileController');
-
-router.get('/profile', userProfileController.getProfile);
-router.post('/profile', userProfileController.saveProfile);
-router.get('/onboarding-status', userProfileController.checkOnboarding);
-
-module.exports = router;
